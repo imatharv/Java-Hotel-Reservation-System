7cd79fb21a875495b3931d83deba34ddc273b3d7
Atharva Joshi <iamatharvaj@google.com>
2021-05-24 08:46:22 +0530

    Initial commit

---
+++ src/main/java/HotelReservationSystem/HotelReservationSystem.java
@@ -0,0 +1,7 @@
+package HotelReservationSystem;
+
+public class HotelReservationSystem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to hotel reservation system");
+	}
+}
